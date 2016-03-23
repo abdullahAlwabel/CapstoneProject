2b1d1d1b1049a63fddd66ff9ee3bec62414982eb
Abdullah Alwabel <tue53200@temple.edu>
2016-03-23 18:29:51 -0400

    swipe screen fixed

--- BootstrapUI/SwipeScreenLatest.aspx.designer.cs
+++ BootstrapUI/SwipeScreenLatest.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace BootstrapUI {
     public partial class WebForm9 {
         
         /// <summary>
+        /// lblTempleSignIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblTempleSignIn;
+        
+        /// <summary>
         /// txtTUID control.
         /// </summary>
         /// <remarks>
@@ -29,5 +38,32 @@ namespace BootstrapUI {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSignIn;
+        
+        /// <summary>
+        /// lblNotTemple control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNotTemple;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
     }
 }
